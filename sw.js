a970a0f700c165d831256d7178f3cb2a7a5c502f
apex-day-app <nakul05012008@gmail.com>
2026-04-17 15:45:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,28 @@
 const CACHE_NAME = 'notes-v1';
 const urlsToCache = [
-  '/my-notes-app/',
   '/my-notes-app/index.html',
-  '/my-notes-app/manifest.json'
+  '/my-notes-app/manifest.json',
+  '/my-notes-app/icon-192.png',
+  '/my-notes-app/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
